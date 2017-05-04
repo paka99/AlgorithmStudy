6b4e053c99250e9f06841da164e9e5030c374651
junseok Lee <rolunoa@gmail.com>
2017-05-04 22:09:50 +0900

    Solve 2017 Round 1A Ratatouille Problem by using Recursive Permutation

--- CodeJam/Stable_Neigh-bors_small.cpp
+++ CodeJam/Stable_Neigh-bors_small.cpp
@@ -1,146 +1,119 @@
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
 #include <vector>
+#include <list>
 #include <map>
-#include <algorithm>
 #include <set>
 #include <queue>
-#include <ctime>
-#include <list>
-#include <cmath>
-#include <string>
+#include <deque>
 #include <stack>
-#include <queue>
-#include <intrin.h>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
 
 using namespace std;
 
 typedef long long int ll;
 typedef unsigned long long int ull;
 
+#define REPEAT(nn) for(int ii = 0; ii < (nn); ii++)
+#define FOR(jj, nn) for(int jj = 0; jj < (nn); (jj)++)
+#define FORe(ii, nn) for(int ii = 1; ii <= nn; ii++)
+#define SIZE(a) ((int)(a).size())
+#define ALL(a) (a).begin(),(a).end()
+
+#define IMP (1 << 30)
+#define NOP (-1)
+
 int T;
 string result;
-int N;
-int numUnicorn[6];
-bool isPossible;
 
-enum { red = 2, blue = 3, yellow = 5, violet = 6, orange = 10, green = 15 };
+int color[6];
+int tot;
 
-int colorArr[] = { 2, 10, 5, 15, 3, 6 };
-
-bool isEmpty(){
-	FOR2(i, 6){
-		if (numUnicorn[i] == 0)	continue;
-		return false;
-	}
-
-	return true;
-}
-
-string convertToString(vector <int> tmpResult){
-	string ret;
-
-	for (int a : tmpResult){
-		switch (a){
-		case 2:
-			ret.push_back('R');
-			break;
-
-		case 3:
-			ret.push_back('B');
-			break;
-
-		case 5:
-			ret.push_back('Y');
-			break;
-
-		case 10:
-			ret.push_back('O');
-			break;
-
-		case 6:
-			ret.push_back('V');
-			break;
-
-		case 15:
-			ret.push_back('G');
-			break;
-		}
-	}
-
-	return ret;
-}
+char colorArr[] = "ROYGBV";
 
 int main(){
 	cin >> T;
-	int maxIdx;
+	ofstream ofs("ans2.txt", ios::trunc);
+	char buffer[5000];
+	//string buffer;
 
-	for (int t = 1; t <= T; t++){
+	FOR(t, T){
 		result.clear();
-		isPossible = true;
 
-		int max1 = -1, max2 = -1;
-		int maxIdx1 = 0, maxIdx2 = 0;
+		cin >> tot;
 
-		cin >> N;
-		FOR2(i, 6)	cin >> numUnicorn[i];
+		int max1 = 0, max2 = 0;
+		int maxCol1 = -1, maxCol2 = -1;
+		int third;
 
-
-		FOR2(i, 6){
-			if (numUnicorn[i] > max1)	{
+		FOR(i, 6)	{
+			cin >> color[i];
+			if (color[i] > max1){
 				max2 = max1;
-				maxIdx2 = maxIdx1;
-				max1 = numUnicorn[i];
-				maxIdx1 = i;
+				maxCol2 = maxCol1;
+				max1 = color[i];
+				maxCol1 = i;
 			}
-			else if (numUnicorn[i] > max2) {
-				max2 = numUnicorn[i];
-				maxIdx2 = i;
+
+			else if (color[i] > max2){
+				max2 = color[i];
+				maxCol2 = i;
 			}
 		}
 
-		int thirdIdx = -1;
+		third = ((maxCol1 + maxCol2) * 2) % 6;
+		int diff = max1 - max2;
+		color[third] -= diff;
+		color[maxCol1] -= diff;
 
-		if (maxIdx1 + maxIdx2 == 6)	thirdIdx = 0;
-		else if (maxIdx1 + maxIdx2 == 2)	thirdIdx = 4;
-		else if (maxIdx1 + maxIdx2 == 4)	thirdIdx = 2;
-
-
-		int d = max1 - max2;
-		if (thirdIdx == -1 || d > numUnicorn[thirdIdx]){
-			isPossible = false;
-			if (!isPossible)	result = string("IMPOSSIBLE");
-			printf("Case #%d: %s\n", t, result.c_str());
+		if (color[third] < 0){
+			sprintf(buffer, "Case #%d: IMPOSSIBLE", t + 1);
+			cout << buffer << endl;
+			ofs << buffer << endl;
 			continue;
 		}
 
-		vector <int> pattern;
+		REPEAT(diff){
+			result += colorArr[maxCol1];
+			result += colorArr[third];
+		}
 
-		FOR2(i, d){
-			pattern.push_back(colorArr[maxIdx1]);
-			pattern.push_back(colorArr[thirdIdx]);
+		diff = color[maxCol2] - color[third];
+		color[maxCol1] -= diff;
+		color[maxCol2] -= diff;
+
+		if (color[third] < 0){
+			sprintf(buffer, "Case #%d: IMPOSSIBLE", t + 1);
+			cout << buffer << endl;
+			ofs << buffer << endl;
+			continue;
+		}
+
+		REPEAT(diff){
+			result += colorArr[maxCol1];
+			result += colorArr[maxCol2];
+		}
+
+		REPEAT(color[maxCol2]){
+			result += colorArr[maxCol1];
+			result += colorArr[maxCol2];
+			result += colorArr[third];
 		}
 
 
-		int d2 = numUnicorn[thirdIdx] - d;
-		FOR2(i, d2){
-			pattern.push_back(colorArr[maxIdx1]);
-			pattern.push_back(colorArr[maxIdx2]);
-			pattern.push_back(colorArr[thirdIdx]);
-		}
-
-
-		int d3 = max2 - d2;
-		FOR2(i, d3){
-			pattern.push_back(colorArr[maxIdx1]);
-			pattern.push_back(colorArr[maxIdx2]);
-		}
-
-		result = convertToString(pattern);
-
-		if (!isPossible)	result = string("IMPOSSIBLE");
-		printf("Case #%d: %s\n", t, result.c_str());
+		sprintf(buffer, "Case #%d: %s", t + 1, result.c_str());
+		cout << buffer << endl;
+		ofs << buffer << endl;
 	}
 
 	return 0;
